17d6baf594752ef02eb8876c1e581d82790abbb9
dajarony <59666460+dajarony@users.noreply.github.com>
2026-09-10 09:13:37 +0100

    feat: export single-phase lighting circuit

--- src/cadesimu_mcp/circuits/__init__.py
+++ src/cadesimu_mcp/circuits/__init__.py
@@ -1,4 +1,9 @@
 from .direct_starter import build_direct_starter_with_control
 from .power import build_three_phase_power_circuit
+from .single_phase_lighting import build_single_phase_lighting_circuit
 
-__all__ = ["build_direct_starter_with_control", "build_three_phase_power_circuit"]
+__all__ = [
+    "build_direct_starter_with_control",
+    "build_single_phase_lighting_circuit",
+    "build_three_phase_power_circuit",
+]
